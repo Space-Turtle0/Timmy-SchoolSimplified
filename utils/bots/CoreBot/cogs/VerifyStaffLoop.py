36b3b8a5e023ea1dd2ef97d119608d4eba3d606f
Aryadeep Buddha <40647186+AryaBuddha@users.noreply.github.com>
2022-02-16 18:41:00 -0500

    Added login handling

--- utils/bots/CoreBot/cogs/VerifyStaffLoop.py
+++ utils/bots/CoreBot/cogs/VerifyStaffLoop.py
@@ -37,7 +37,7 @@ class VerifyStaffLoop:
         self.verifyStaff.start()
         self.staffrole = 932066545117585430
         self.logchannel = 932066545885134904
-        #TODO: Make userable for rolesync and different servers
+        #TODO: Make useable for rolesync and different servers
     
     def cog_unload(self):
         pass
@@ -50,34 +50,50 @@ class VerifyStaffLoop:
             AttributeNames=[
                 'All'
             ],
-            MaxNumberOfMessages=10,
-            VisibilityTimeout=123,
+            MessageAttributeNames=[
+                'All'
+            ],
+            MaxNumberOfMessages=1,
+            VisibilityTimeout=0,
+            WaitTimeSeconds=1
         )
         guild = TECH_ID.g_tech
         
+        try:
+            for message in responses["Messages"]:
 
-        for message in responses["Messages"]:
+                user_data = json.loads(message["Body"])
+                user_data = user_data["MessageAttributes"]
 
-            user_data = json.loads(message["Body"])
-            
-            embed = discord.Embed(title = "Authentication Successful", description = f"Hello {user_data['name']}, you have successfully authenticated with your GSuite account, {user_data['email']}.", color = discord.Color.green())
-            embed.set_footer(text = "Assigning you your staff role.")
+                discord_id = int(user_data["discord_id"]["StringValue"])
+                email = user_data["email"]["StringValue"]
+                first = user_data["first"]["StringValue"]
+                last = user_data["last"]["StringValue"]
 
-            user_id = int(user_data["discordID"])
 
-            member: discord.Member = guild.get_member(user_id)
-            role = guild.get_role(self.staffrole)
-            await member.add_roles(role, reason=f"Passed authentication: {user_data['email']}")
+                embed = discord.Embed(title = "Authentication Successful", description = f"Hello {first}, you have successfully authenticated with your GSuite account, {email}.", color = discord.Color.green())
+                embed.set_footer(text = "Assigning you your staff role.")
 
-            now = datetime.now()
-            now = now.astimezone(pytz.timezone('US/Eastern')).strftime('%m/%d/%Y %I:%-M %p')
-            logchannel: discord.TextChannel = await self.bot.fetch_channel(self.logchannel)
+                user_id = int(discord_id)
 
-            embed = discord.Embed(title = "GSuite Authentication", description = f"{member.mention} has successfully authenticated with their GSuite account.", color = discord.Color.green())
-            embed.add_field(name = "Verification Details", value = "**Name:** {}\n**Email:** {}\n**Date:** {} EST".format(f"{user_data['name']} {user_data['lastName']}", user_data['email'], now))
-            embed.set_footer(text = "This action was performed by {}".format(member.display_name))
-            await logchannel.send(embed=embed)
+                member: discord.Member = guild.get_member(user_id)
+                DMChannel = await member.create_dm()
+                DMChannel.send(embed = embed)
 
-            
-            response = SQS_CRED.sqs_client.delete_message(
-                QueueUrl=SQS_CRED.SQS_URL,ReceiptHandle=message["ReceiptHandle"])
+                role = guild.get_role(self.staffrole)
+                await member.add_roles(role, reason=f"Passed authentication: {email}")
+
+                now = datetime.now()
+                now = now.astimezone(pytz.timezone('US/Eastern')).strftime('%m/%d/%Y %I:%-M %p')
+                logchannel: discord.TextChannel = await self.bot.fetch_channel(self.logchannel)
+
+                embed = discord.Embed(title = "GSuite Authentication", description = f"{member.mention} has successfully authenticated with their GSuite account.", color = discord.Color.green())
+                embed.add_field(name = "Verification Details", value = "**Name:** {}\n**Email:** {}\n**Date:** {} EST".format(f"{first} {last}", email, now))
+                embed.set_footer(text = "This action was performed by {}".format(member.display_name))
+                await logchannel.send(embed=embed)
+
+                
+                SQS_CRED.sqs_client.delete_message(QueueUrl=SQS_CRED.SQS_URL,ReceiptHandle=message["ReceiptHandle"])
+        
+        except KeyError:
+            pass
--- utils/bots/CoreBot/cogs/verifystaffGsuite.py
+++ utils/bots/CoreBot/cogs/verifystaffGsuite.py
@@ -22,6 +22,7 @@ class GSuiteLogin(commands.Cog):
         self.staffrole = 932066545117585430
         self.logchannel = 932066545885134904
         self.est = pytz.timezone("US/Eastern")
+        self.verify_link = "https://timmy-gsuite-verify.vercel.app" #TODO: Add real link
 
     @commands.Cog.listener("on_interaction")
     async def GSuiteVerify(self, interaction: discord.Interaction):
@@ -37,24 +38,17 @@ class GSuiteLogin(commands.Cog):
         if (
             InteractionResponse["custom_id"] == "persistent_view:gsuiteverify"
         ):
-            def check(m):
-                return (
-                    m.content is not None
-                    and m.channel == DMChannel
-                    and m.author.id is author.id
-                )
-            
+          
             channel = await self.bot.fetch_channel(interaction.channel_id)
             guild = interaction.message.guild
             author = interaction.user
             DMChannel = await author.create_dm()
 
-            auth_url, _ = self.flow.authorization_url(prompt='consent')
-            auth_url = auth_url + "&hb=schoolsimplified.org"
+            auth_url = self.verify_link + "?discordID=" + str(author.id)
             #auth_url = auth_url.replace('auth', 'auth/oauthchooseaccount', 1)
-            embed = discord.Embed(title = "GSuite Verification", description = "Click on the button below and sign in with your **personal** @schoolsimplified.org account.\nOnce you sign in you should get a code, copy and paste that here.", color = discord.Color.red())
+            embed = discord.Embed(title = "GSuite Verification", description = "Click on the button below and sign in with your **personal** @schoolsimplified.org account.", color = discord.Color.red())
             embed.add_field(name = "Getting Error Code '403: org_internal'?", value = "Make sure you sign in with your @schoolsimplified.org **Google Account**. If you are immediately being redirected to that webpage, make sure you sign in on your browser first and then try clicking the verification button again. ")
-            embed.set_footer(text = "NOTE: This token is not stored and is only used to determine your identity.")
+            
             
             view = discord.ui.View()
             emoji = Emoji.timmyBook
@@ -68,51 +62,9 @@ class GSuiteLogin(commands.Cog):
                 )
             )
             await DMChannel.send(embed=embed, view=view)
-            await DMChannel.send("Now paste your authentication code here:")
-
-            try:
-                answer1 = await self.bot.wait_for("message", timeout=200.0, check=check)
-                if (
-                    answer1.content is None
-                    or answer1.content == ""
-                    or answer1.content == " "
-                ):
-                    return await DMChannel.send(
-                        "No message was sent, try again later..."
-                    )
-                else:
-                    try:
-                        self.flow.fetch_token(code=answer1.content)
-                    except Exception as e:
-                        return await DMChannel.send(
-                            "Invalid code, try again later..."
-                        )
-                    else:
-                        session = self.flow.authorized_session()
-                        respjson = session.get('https://www.googleapis.com/userinfo/v2/me').json()
-                        firstname = respjson["given_name"]
-                        if firstname == "SS" or firstname == "PS":
-                            return await DMChannel.send("You are not allowed to authenticate with Department Accounts, please use your **personal** account to authenticate.")
-                        else:
-                            embed = discord.Embed(title = "Authentication Successful", description = f"Hello {firstname}, you have successfully authenticated with your GSuite account, {respjson['email']}.", color = discord.Color.green())
-                            embed.set_footer(text = "Assigning you your staff role.")
-                            await DMChannel.send(embed=embed)
-
-                            member: discord.Member = guild.get_member(author.id)
-                            role = guild.get_role(self.staffrole)
-                            await member.add_roles(role, reason = "Passed Verification: {}".format(respjson["email"]))
-
-                            now = datetime.now()
-                            now = now.astimezone(pytz.timezone('US/Eastern')).strftime('%m/%d/%Y %I:%-M %p')
-                            logchannel: discord.TextChannel = await self.bot.fetch_channel(self.logchannel)
-
-                            embed = discord.Embed(title = "GSuite Authentication", description = f"{member.mention} has successfully authenticated with their GSuite account.", color = discord.Color.green())
-                            embed.add_field(name = "Verification Details", value = "**Name:** {}\n**Email:** {}\n**Date:** {} EST".format(f"{respjson['given_name']} {respjson['family_name']}", respjson["email"], now))
-                            embed.set_footer(text = "This action was performed by {}".format(member.display_name))
-                            await logchannel.send(embed=embed)
             
-            except asyncio.TimeoutError:
-                await DMChannel.send("Looks like you didn't respond in time, please try again later!")
+
+            
 
     @commands.command()
     @is_botAdmin
